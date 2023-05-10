95552ce891f84d9e766ee25c26e8d305e9b9bbb4
aikirer <witoldbrzusz@wp.pl>
2023-05-10 20:37:49 +0200

    remove repl

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let args = args().collect::<Vec<String>>();
     match args.len() {
         2 => apla::run_file(&args[1]),
-        1 => apla::repl(),
+        // 1 => apla::repl(),
         _ => {
             eprintln!("Usage: apla <file-name>");
             std::process::exit(1);
